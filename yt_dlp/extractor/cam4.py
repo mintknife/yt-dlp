807fb3df2e787de5523a1a4a0d8032eab65b2933
mintknife <mintknife@protonmail.com>
2025-12-06 16:41:53 +0100

    fix(cam4): Improve error handling for offline/not-streaming performers

--- yt_dlp/extractor/cam4.py
+++ yt_dlp/extractor/cam4.py
@@ -1,4 +1,5 @@
 from .common import InfoExtractor
+from ..utils import ExtractorError
 
 
 class CAM4IE(InfoExtractor):
@@ -17,7 +18,33 @@ class CAM4IE(InfoExtractor):
 
     def _real_extract(self, url):
         channel_id = self._match_id(url)
-        m3u8_playlist = self._download_json(f'https://www.cam4.com/rest/v1.0/profile/{channel_id}/streamInfo', channel_id).get('cdnURL')
+
+        # First check if the performer exists and their online status
+        profile_info = self._download_json(
+            f'https://www.cam4.com/rest/v1.0/profile/{channel_id}/info',
+            channel_id, fatal=False, expected_status=404)
+
+        if not profile_info:
+            raise ExtractorError(f'{channel_id}: Performer not found', expected=True)
+
+        is_online = profile_info.get('online', False)
+
+        if not is_online:
+            raise ExtractorError(f'{channel_id}: Performer is currently offline', expected=True)
+
+        # Performer is online, try to get stream info
+        stream_info = self._download_json(
+            f'https://www.cam4.com/rest/v1.0/profile/{channel_id}/streamInfo',
+            channel_id, fatal=False, expected_status=(204, 404))
+
+        if not stream_info:
+            raise ExtractorError(
+                f'{channel_id}: Performer is online but not currently streaming', expected=True)
+
+        m3u8_playlist = stream_info.get('cdnURL')
+        if not m3u8_playlist:
+            raise ExtractorError(
+                f'{channel_id}: Stream info found but no playlist URL available', expected=True)
 
         formats = self._extract_m3u8_formats(m3u8_playlist, channel_id, 'mp4', m3u8_id='hls', live=True)
 
